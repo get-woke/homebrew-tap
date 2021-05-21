07fa9fe96b632e30c0c80fb0267be8b39ef12372
goreleaserbot <goreleaser@carlosbecker.com>
2021-05-21 16:21:40 -0400

    Brew formula update for woke version v0.8.1

--- Formula/woke.rb
+++ Formula/woke.rb
@@ -5,16 +5,16 @@
 class Woke < Formula
   desc "Detect non-inclusive language in your source code."
   homepage "https://getwoke.tech"
-  version "0.8.0"
+  version "0.8.1"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/get-woke/woke/releases/download/v0.8.0/woke-0.8.0-darwin-amd64.tar.gz"
-    sha256 "80747290e5d8ed74fbb1f870e9f382daada926f537606230df6ee9108cbc25d8"
+    url "https://github.com/get-woke/woke/releases/download/v0.8.1/woke-0.8.1-darwin-amd64.tar.gz"
+    sha256 "cbf04533af2b3d4e6fabd17debbcfb2bf6342baae0d80b2adc9b84fd3902d90b"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/get-woke/woke/releases/download/v0.8.0/woke-0.8.0-linux-amd64.tar.gz"
-    sha256 "e44e5c422731ef9b2575c48fbebb69d16ed70eeaa1858505591b90aa55282324"
+    url "https://github.com/get-woke/woke/releases/download/v0.8.1/woke-0.8.1-linux-amd64.tar.gz"
+    sha256 "57773a297ebbf8c5008a5230f681d2574329d48dbe43c4968e1107fa0fb803cf"
   end
 
   def install
